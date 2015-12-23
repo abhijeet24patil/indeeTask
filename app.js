d2fb4cc61165a1eeaf39a7682616ac7c7b4c63c0
Techbuds <abhijeet.patil@tekbuds.com>
2015-12-23 12:27:47 +0530

    Abhijeet- Modified all the files

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-angular.module('vorisApp', ["ui.bootstrap"])
-    .controller('vorisCtrl', function($scope, $http) {
-        $scope.ArrinfoData = [];
-         
+angular.module('indeeApp', ["ui.bootstrap"])
+    .controller('videoCtrl', function($scope, $http) {
+        $scope.ArrinfoData = []; //to store objects
+
         $http.get('data.json').success(function(data) {
-            $scope.information  =  data;
-           // console.log($scope.information);
-            angular.forEach($scope.information,function(value,key){
-                $scope.infoData = {};
-                $scope.infoData['ip'] = key; 
+            $scope.information = data;
+            angular.forEach($scope.information, function(value, key) {
+                $scope.infoData = {}; //object to get new values for data
+                $scope.infoData['ip'] = key;
                 for (var i = 0; i < value.length; i++) {
+                    //copy each object from json and put it in new object and then push to new array
                     $scope.infoData['timestamp'] = value[i].timestamp;
                     $scope.infoData['fullView'] = value[i].full_view;
                     $scope.infoData['seek'] = value[i].seek;
                     $scope.infoData['fullLength'] = value[i].full_length;
-                    $scope.sBar  =  [];
-
-                   if(value[i].seek != null){
-                        $scope.bar  =  {};
-                        $scope.bar["value"]  =  (parseInt(value[i].seek[0][0])/parseInt(value[i].full_length))*100;
-                        $scope.bar["type"]  =  "success";
+                    $scope.infoData['ontime'] = value[i].ontime;
+                    $scope.sBar = [];
+                    //if seek is present in object then calculate the seek time and view time.
+                    if (value[i].seek != null) {
+                        //adds viewed part to display-bar with green color
+                        $scope.bar = {};
+                        $scope.bar["value"] = (parseInt(value[i].seek[0][0]) / parseInt(value[i].full_length)) * 100;
+                        $scope.bar["type"] = "success";
                         $scope.sBar.push($scope.bar);
-                        $scope.bar  =  {};
-                        
-                        console.log(value[i].seek.length);
-                        for (var j = 0; j<value[i].seek.length; j++) {
-                             $scope.bar  =  {};    
-                             $scope.bar["value"]  =  ((parseInt(value[i].seek[j][1])-parseInt(value[i].seek[j][0])) /parseInt(value[i].full_length))*100;
-                             $scope.bar["type"]  =  "default";
-                             $scope.sBar.push($scope.bar);                             
+                        $scope.bar = {};
+
+                        //adds the seek value to display-bar with grey color
+                        for (var j = 0; j < value[i].seek.length; j++) {
+                            $scope.bar = {};
+                            $scope.bar["value"] = ((parseInt(value[i].seek[j][1]) - parseInt(value[i].seek[j][0])) / parseInt(value[i].full_length)) * 100;
+                            $scope.bar["type"] = "default";
+                            $scope.sBar.push($scope.bar);
                         };
 
-                        for (var k = 1; k<value[i].seek.length; k++) {
-                             $scope.bar  =  {};    
-                             $scope.bar["value"]  =  ((parseInt(value[i].seek[(k)][0])-parseInt(value[i].seek[(k-1)][1]))/parseInt(value[i].full_length))*100;
-                             $scope.bar["type"]  =  "success";
-                             $scope.sBar.push($scope.bar);                             
+                        //adds the viewed part(betwwen 2 seeks) to the display-bar with green color  
+                        for (var k = 1; k < value[i].seek.length; k++) {
+                            $scope.bar = {};
+                            $scope.bar["value"] = ((parseInt(value[i].seek[(k)][0]) - parseInt(value[i].seek[(k - 1)][1])) / parseInt(value[i].full_length)) * 100;
+                            $scope.bar["type"] = "success";
+                            $scope.sBar.push($scope.bar);
                         };
 
-
-
-                        $scope.infoData['seek'] =$scope.sBar;
+                        $scope.infoData['seek'] = $scope.sBar;
                     }
 
                 };
-                 $scope.ArrinfoData.push($scope.infoData);
-                
+                //push each object to array 
+                $scope.ArrinfoData.push($scope.infoData);
+
             })
         });
-            //console.log($scope.sBar);
         console.log($scope.ArrinfoData);
-         $scope.stacked = [{"value":23.69,"type":"info"},{"value":27,"type":"success"},{"value":27,"type":"danger"}];
     });
