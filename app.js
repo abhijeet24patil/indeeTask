eb7df5b35e91f262722e92cd4147bb92ad9c771c
Techbuds <abhijeet.patil@tekbuds.com>
2015-12-29 11:28:04 +0530

    abhijeet-updated

--- app.js
+++ app.js
@@ -67,11 +67,18 @@ angular.module('indeeApp', ["ui.bootstrap"])
             //To clear the form fields
         $scope.clear = function() {
             $scope.video = {};
-
         }
 
         $scope.remove = function(index) {
             $scope.ArrinfoData.splice(index, 1);
         }
+
+        $scope.sampleData= {
+                     "204.138.240.254": [{"full_length": "160.149", "full_view": true, "timestamp": "2015-12-21 22:50:13", "ontime": "55", "complete": "159.93"}],
+                     "216.52.215.232": [{"full_length": "160.149", "full_view": true, "timestamp": "2015-12-22 00:04:51", "ontime": "115", "complete": "160.01"}],
+                     "64.239.129.250": [{"full_length": "160.149", "timestamp": "2015-12-21 22:21:46", "seek": [["12.17", "17.31022279411765"],["18.17", "28.31022279411765"]], "pause": "18.05", "ontime": "18.05"}],
+                     "64.15.218.74":[{"timestamp": "2015-12-15 20:55:30", "pause": "25.69", "ontime": "180", "seek": [["25.69","78.02891724617524"], ["89.54", "127.31033866481224"]], "full_length": "5905.557"}
+                     ]
+                     };
 
     });
